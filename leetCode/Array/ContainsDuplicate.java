74e8251ad76fad5448f6ee108bcd6f33b865a834
toshalSelokar <toshalselokar@gmail.com>
2025-08-11 11:25:52 +0530

    Add files via upload

---
+++ leetCode/Array/ContainsDuplicate.java
@@ -0,0 +1,50 @@
+package leetCode.Array;
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+Constraints:
+
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+ */
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        int i = 0;
+        while(i < nums.length){
+            if(set.contains(nums[i])){
+                return true;
+            } else{
+                set.add(nums[i]);
+            }
+
+            i++;
+        }
+        return false;
+    }
+}
